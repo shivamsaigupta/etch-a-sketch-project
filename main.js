dc3dc786c64b0202b46fa0b76e35cd6427cbee08
DESKTOP-MSG9PH7\Shivam <shivam@shivam.me>
2019-12-21 12:24:06 +0530

    Added the ability to change the number of pixels.

--- main.js
+++ main.js
@@ -1,23 +1,51 @@
-sketchpad = document.querySelector('#sketchpad');
 
-const NUM_PIXELS = 32;
+const defaultNumOfPixels = 16;
 
-for(let i=0; i<NUM_PIXELS; i++){
-  //Create pixel row
-  pixelRow = document.createElement('div');
-  pixelRow.classList.add('pixelRow');
-  sketchpad.appendChild(pixelRow);
+let sketchpad = document.querySelector('#sketchpad');
+drawPixels(defaultNumOfPixels);
 
-  for(let j=0; j<NUM_PIXELS; j++){
-    pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    pixelRow.appendChild(pixel);
+function drawPixels(numOfPixels){
+  for(let i=0; i<numOfPixels; i++){
+    //Create pixel row
+    pixelRow = document.createElement('div');
+    pixelRow.classList.add('pixelRow');
+    sketchpad.appendChild(pixelRow);
+
+    for(let j=0; j<numOfPixels; j++){
+      pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      pixelRow.appendChild(pixel);
+    }
+  }
+
+  pixels = document.querySelectorAll('.pixel');
+  pixels.forEach(myPixel => {
+    myPixel.addEventListener('mouseenter', ()=> {
+      myPixel.style.cssText = "background-color: #C9B7C9;";
+    })
+  })
+}
+
+
+
+changePixelBtn = document.querySelector('.button');
+changePixelBtn.addEventListener('click', onChangePixelsTap);
+
+function onChangePixelsTap(){
+  let input = prompt('Specify the size of grids. Example: 16 for a 16x16 grid.')
+  input = +input;
+  if(typeof(input) != 'number' || input === 0){
+    return;
+  }else{
+    resetSketchPad();
+    console.log(input);
+    drawPixels(input);
   }
 }
 
-pixels = document.querySelectorAll('.pixel');
-pixels.forEach(myPixel => {
-  myPixel.addEventListener('mouseenter', ()=> {
-    myPixel.style.cssText = "background-color: black;";
+function resetSketchPad(){
+  pixelRows = document.querySelectorAll('.pixelRow');
+  pixelRows.forEach(myPixelRow => {
+    sketchpad.removeChild(myPixelRow);
   })
-})
+}
